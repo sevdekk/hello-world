78beff7b2c5058f85593ccbebfa28384e0232c30
sevdekk <79819539+sevdekk@users.noreply.github.com>
2021-02-28 20:24:33 -0700

    FirstBranchUpdates
    a. Line 2 is edited to add my name.
    b. addNumber() : num + previousNumber added.
    c. reverseNumber(): private double reverseNum is added to GitDemo class.
    reverseNum = newNumber is added in the reverseNumber() function.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Sevde Kumral
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        return num + previousNumber; //??? not sure !
     }
     
     /*****
@@ -40,6 +41,9 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
+        
+        reverseNum = newNumber;
+        
         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
